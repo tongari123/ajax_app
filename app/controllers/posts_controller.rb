c3dd74714dc09fd8d50a3102d7965553f061cdc8
tongari123 <108406294+tongari123@users.noreply.github.com>
2022-08-12 09:35:19 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index  # 追記する
   end
-end
+ 
+ end
+ 
